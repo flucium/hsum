e53bb443c3d92957720a1204fdd4dc50156dde9a
flucium <14249224+flucium@users.noreply.github.com>
2023-10-09 13:18:40 +0900

    add ./src/manifest.rs

---
+++ src/manifest.rs
@@ -0,0 +1,7 @@
+pub const NAME: &str = "HSum";
+
+pub const VERSION: &str = "0.0.1";
+
+pub const AUTHOR: &str = "flucium <flucium@flucium.net>";
+
+pub const ABOUT: &str = "A simple cli tool get hash digest from stdin";
